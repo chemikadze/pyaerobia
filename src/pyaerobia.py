faf2ab77a83852d7722acf4d6f1169cb88b251e6
Nikolay Sokolov <chemikadze@gmail.com>
2013-12-08 02:34:24 +0400

    Paging support, month names fix

--- src/pyaerobia.py
+++ src/pyaerobia.py
@@ -20,9 +20,9 @@ class Workout(object):
         self.length = length
 
     def __repr__(self):
-        return 'Workout(' + \
-               '%(id)s, %(name)s, %(date)s, %(duration)s, %(length)s' + \
-               ')' % self.__dict__
+        return (('Workout(' +
+                 '%(id)s, %(name)s, %(date)s, %(duration)s, %(length)s' +
+                 ')') % self.__dict__).encode('utf-8')
 
 
 class Aerobia(object):
@@ -37,10 +37,14 @@ class Aerobia(object):
     def _auth_url(self):
         return urljoin(self.__root, '/users/sign_in')
 
-    def _workouts_url(self, user_id):
-        return urljoin(
+    def _workouts_url(self, user_id, page=None):
+        base = urljoin(
             self.__root,
             '/users/%(user_id)s/workouts?view=list' % locals())
+        if page is not None:
+            return base + '&page=' + str(page)
+        else:
+            return base
 
     def _export_url(self, workout_id, fmt):
         return urljoin(
@@ -68,7 +72,7 @@ class Aerobia(object):
         assert response.getcode() / 100 == 2
         soup = BeautifulSoup(response.read())
         profile_tags = soup.findAll(name='li', attrs={'class': 'profile'})
-        self._user_id = profile_tags[0].a['href'].split('/')[-1]
+        self._user_id = profile_tags[0].a['href'].split('/')[-1].split('?')[0]
 
     def _flatten_strings(self, tag):
         contents = []
@@ -81,8 +85,8 @@ class Aerobia(object):
 
     def _month_num(self, month):
         months = [u'январь', u'февраль',
-                  u'март', u'апрель', u'май',
-                  u'июнь', u'июль', u'август',
+                  u'марта', u'апрель', u'мая',
+                  u'июня', u'июля', u'август',
                   u'сентябрь', u'октябрь', u'ноябрь',
                   u'декабрь']
         prefix = month.strip('.')
@@ -96,22 +100,37 @@ class Aerobia(object):
         self._do_auth(user, password, token)
 
     def workout_list(self, user_id=None):
+        return list(self.workout_iterator(user_id))
+
+    def workout_iterator(self, user_id=None):
+        page = 0
+        while True:
+            current_page_workouts = self._workout_page(page, user_id)
+            if not len(current_page_workouts):
+                return
+            for w in current_page_workouts:
+                yield w
+            page += 1
+
+    def _workout_page(self, page, user_id=None):
         user_id = user_id or self._user_id
         request = Request(
-            url=self._workouts_url(user_id),
+            url=self._workouts_url(user_id, page),
             headers=self._CHEAT_HEADERS)
         response = self._opener.open(request)
         soup = BeautifulSoup(response.read())
         tables = soup.findAll("table", attrs={'class': 'list'})
+        if not len(tables):  # TODO: check this is really empty page
+            return []
         workout_rows = tables[0].tbody.findAll("tr")
 
         workouts = []
         for tr in workout_rows:
             name_tds = tr.findAll('td', attrs={'class': 'title'})
-            name = name_tds[0].div['title']
+            name = (name_tds[0].string or name_tds[0].div['title']).strip()
 
             id_refs = tr.findAll('a', attrs={'data-partial': 'workout'})
-            id = id_refs[0]['href'].split('/')[-1]
+            id = id_refs[0]['href'].split('/')[-1].split('?')[0]
 
             datetime_spans = tr.findAll('span', attrs={'class': 'datetime'})
             datetime_str = \
@@ -123,17 +142,20 @@ class Aerobia(object):
 
             duration_td = tr.findAll('td')[-1]
             duration_str = duration_td.string
-            duration_re = re.compile(r'(\d+)\D+(\d+)\D+(\d+)')
-            [d_h, d_m, d_s] = duration_re.match(duration_str).groups()
-            duration = \
-                timedelta(hours=int(d_h), minutes=int(d_m), seconds=int(d_s))
+            duration_re = re.compile(r'((\d+)\D+)?(\d+)\D+(\d+)')
+            [_, d_h, d_m, d_s] = duration_re.match(duration_str).groups()
+            duration = timedelta(
+                hours=int(d_h or 0),
+                minutes=int(d_m),
+                seconds=int(d_s))
 
             length_tds = tr.findAll(lambda el: (el.string or '').count(u'км'))
             length_str = length_tds[0].string
             length_re = re.compile(r'([0-9.]+).*')
             length = float(length_re.match(length_str).group(1))
 
-            workouts.append(Workout(id, name, date, duration, length))
+            workout = Workout(id, name, date, duration, length)
+            workouts.append(workout)
 
         return workouts
 
